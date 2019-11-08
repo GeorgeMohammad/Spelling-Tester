9e6aa73872617dd4c33b17f50a4af05ddb87a3bf
GeorgeMohammad <38092421+GeorgeMohammad@users.noreply.github.com>
2019-11-07 21:31:44 -0600

    The file includes code to read data from a file into a list.

---
+++ File2List.py
@@ -0,0 +1,6 @@
+dictionaryFile = open("words.txt", "r")
+dictionary = dictionaryFile.read()
+dictionaryList = dictionary.split("\n")
+##print(dictionaryList[2:5])
+##for i in range(100):
+##    print(dictionaryList[i])
