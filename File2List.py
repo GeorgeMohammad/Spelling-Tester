bb92a9dbc8638f2c8010510b330d003d48d7b56c
GeorgeMohammad <38092421+GeorgeMohammad@users.noreply.github.com>
2019-11-08 13:45:59 -0600

    Improved File2List.py
    I wrapped the file reader code into a function in a class.
    I added a new method to the same class as above.
    I added error checking for the above two items.

--- File2List.py
+++ File2List.py
@@ -1,6 +1,40 @@
-dictionaryFile = open("words.txt", "r")
-dictionary = dictionaryFile.read()
-dictionaryList = dictionary.split("\n")
-##print(dictionaryList[2:5])
-##for i in range(100):
-##    print(dictionaryList[i])
+
+
+class FileProcessing:
+#Reads data from a file into a variable.
+    def FileReader(self, fileName):
+        try:
+            open(fileName, "r")
+        except IOError:
+            print("Invalid file name.")
+        else:
+            fileI = open(fileName, "r")
+            fileContents = fileI.read()
+            return fileContents
+
+#fileContents is a string
+#Splits fileContents into a list at the character, delimiter.
+    def FileSplitter (self, fileContents, delimiter):
+        try:
+            if type(fileContents) is not str:
+                print(type(fileContents))
+                raise ArgumentException1
+            
+        except Exception as ArgumentException1:
+            print("Must enter a string for fileContents.")
+        else:
+            try:
+                if ((type(delimiter) is not str) or (delimiter == "")):
+                    raise ArgumentException2
+            except Exception as ArgumentException2:
+                print("Invalid Delimiter")
+            else:
+                return fileContents.split(delimiter)
+
+#Test code
+##obj = FileProcessing()
+##
+##print(obj.FileSplitter("s s", ""))
+##data = obj.FileReader("words1.txt")
+##
+##
