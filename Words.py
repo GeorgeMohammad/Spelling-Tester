d9416cb03a04513ae4acd4ba58362d2e77a2ed90
GeorgeMohammad <38092421+GeorgeMohammad@users.noreply.github.com>
2019-11-10 20:20:11 -0600

    I added error checking.

--- Words.py
+++ Words.py
@@ -2,8 +2,13 @@ import Random
 class Words:
     def getRandVal(self, targetList):
         randNumObj = Random.Random()
-        randNum = randNumObj.randNumMaker(0, len(targetList))
-        return targetList[randNum]
-
+        try:
+            if type(targetList) is not list:
+                raise Exception
+        except:
+            print("Enter a value of type list.")
+        else:
+            randNum = randNumObj.randNumMaker(0, len(targetList))
+            return targetList[randNum]
 
         
