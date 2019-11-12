4fc88b237501b6744ce8ac1b9d05a0d5dd59c909
GeorgeMohammad <38092421+GeorgeMohammad@users.noreply.github.com>
2019-11-11 20:53:21 -0600

    I added error checking and test cases.

--- Random.py
+++ Random.py
@@ -1,7 +1,25 @@
 import random
 class Random:
     def randNumMaker(self, startInt, stopInt):
-        randomNum = random.randrange(startInt, stopInt)
-        return randomNum
+        try:
+            if ((type(startInt) is not int) or (type(stopInt) is not int)):
+                raise Exception
+        except:
+            print("Error. One or more arguments is not an integer.")
+        else:
+            randomNum = random.randrange(startInt, stopInt)
+            return randomNum
 
-
+#Test code
+##randObj = Random()
+##print(randObj.randNumMaker(1, 20))
+##print(randObj.randNumMaker("f", 20))
+##print(randObj.randNumMaker(1, "f"))
+##print(randObj.randNumMaker("f", "f"))
+##print(randObj.randNumMaker(1.0, 20.0))
+##print(randObj.randNumMaker(1.0, 20))
+##print(randObj.randNumMaker(1, 20.0))
+##print(randObj.randNumMaker(True, 20))
+##print(randObj.randNumMaker(1, True))
+##print(randObj.randNumMaker(True, True))
+##print(randObj.randNumMaker(1, 20))
