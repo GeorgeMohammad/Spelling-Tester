6c01a3558b4c0a680f915300e8aedc528c2aa92b
GeorgeMohammad <38092421+GeorgeMohammad@users.noreply.github.com>
2019-11-11 20:55:41 -0600

    I added exception handling.

--- Random.py
+++ Random.py
@@ -1,7 +1,11 @@
 import random
 class Random:
     def randNumMaker(self, startInt, stopInt):
-        randomNum = random.randrange(startInt, stopInt)
-        return randomNum
-
-
+        try:
+            if ((type(startInt) is not int) or (type(stopInt) is not int)):
+                raise Exception
+        except:
+            print("Error. One or more arguments is not an integer.")
+        else:
+            randomNum = random.randrange(startInt, stopInt)
+            return randomNum
